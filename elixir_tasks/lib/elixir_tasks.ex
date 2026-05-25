a6b7a44002a3bcc0477959b8f15d45cf43f0f5e2
gamahyss <knuxko@MacBook-Air-Alexandra.local>
2026-05-25 10:00:18 +0300

    .

---
+++ elixir_tasks/lib/elixir_tasks.ex
@@ -0,0 +1,18 @@
+defmodule ElixirTasks do
+  @moduledoc """
+  Documentation for `ElixirTasks`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirTasks.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
